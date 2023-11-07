9fe49afd70793b2f78d4e5945adbe37f255e0a3b
Truong-Quang-Dat <100523403+Truong-Quang-Dat@users.noreply.github.com>
2023-11-07 17:17:09 +0700

    update login UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   AOS.init();
@@ -22,7 +23,7 @@ window.addEventListener('scroll', () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path="/footer" element={<Footer />} />
-
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
