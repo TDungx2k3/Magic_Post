6d492f686add47a5ede23dee65dd7b2ad99d6ee2
TDungx2k3 <dungpgpma2k3@gmail.com>
2023-12-19 22:43:12 +0700

    add qrcode reader

--- frontend/src/pages/Customer/index.js
+++ frontend/src/pages/Customer/index.js
@@ -4,9 +4,45 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import clsx from "clsx";
 import axios from "axios";
+import jsQR, { QRCodeReader } from 'jsqr';
+import QRCode from "qrcode.react";
 
 function CustomerQR() {
   const [orderIdCode, setOrderIdCode] = useState("");
+  const [result, setResult] = useState('');
+  
+  const handleImageLoad = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const img = new Image();
+        img.onload = () => {
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0, img.width, img.height);
+
+          const imageData = context.getImageData(0, 0, img.width, img.height);
+          const code = jsQR(imageData.data, img.width, img.height);
+
+          if (code) {
+            console.log(code.data);
+            setResult(code.data);
+          } else {
+            console.log("Không tìm thấy mã QR.");
+            setResult('Không tìm thấy mã QR.');
+          }
+        };
+
+        img.src = event.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
 
   const handleInputChange = (e) => {
     setOrderIdCode(e.target.value);
@@ -56,6 +92,9 @@ function CustomerQR() {
           name="orderIdCode"
           onChange={handleInputChange}
         />
+
+        <input type="file" accept="image/*" onChange={handleImageLoad} />
+
         <button className={style.orderCodeBtn} onClick={handleSubmit}>
           Search your order information
         </button>
